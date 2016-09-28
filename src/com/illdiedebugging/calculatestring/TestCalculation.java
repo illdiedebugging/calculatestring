25058d1b0b2bf830e1cf98dc6b49708c53cf5d91
illdiedebugging <tzay299j@protonmail.com>
2016-09-29 00:01:06 +0300

    Update TestCalculation.java

--- src/com/illdiedebugging/calculatestring/TestCalculation.java
+++ src/com/illdiedebugging/calculatestring/TestCalculation.java
@@ -4,11 +4,11 @@ public class TestCalculation {
 	public static void main(String args[]){
 		String str = null;
 		try{
-		str = Calculate.getResult("1-((100/5+4*2)+5)*3*(100)-(10*10^2)");
+		str = Calculate.getResult("1-((100.5443/5+4.12333*2)+5)*√3(-100)-(10*10^2)"); //Result must be -6714.767664977295
+		//str = Calculate.getResult("(3.345+4.567)(2+5)"); //Result must be 55.38400000000001
 		}catch(UnableToCalculateException e) {
 			System.out.println(e);
 		}
-		
-		System.out.println(str); //Result must be -10899
+		System.out.println(str); 
 	}
 }
